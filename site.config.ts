ec57ce064750c5c229c2a24a04e7a68bcae55e29
黄柏特 <106975133+KashiwaByte@users.noreply.github.com>
2022-07-05 10:48:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b9e9f47bea08482f8d61c2a865751678',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'KashiwaByte's Blog',
+  domain: 'https://equal-trowel-ddc.notion.site/Blog-b9e9f47bea08482f8d61c2a865751678',
+  author: 'KashiwaByte',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  Wechat: '15686221517',
+  github: 'KashiwaByte',
+  QQ: '471314513',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
